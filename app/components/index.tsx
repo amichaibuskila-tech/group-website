efcf660d4ed6cb60e9e0ec25a9453d054fc65c04
Amichai Buskila <amichai.buskila@gmail.com>
2026-03-27 12:57:05 +0300

    Refactor component imports and update package dependencies; remove unused SVG files

---
+++ app/components/index.tsx
@@ -0,0 +1,10 @@
+export { default as About } from './About';
+export { default as Achievements } from './Achievements';
+export { default as Footer } from './Footer';
+export { default as Hero } from './Hero';
+export { default as Lab } from './Lab';
+export { default as Members } from './Members';
+export { default as Navbar } from './Navbar';
+export { default as Sponsors } from './Sponsors';
+
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,4 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Lab from "./components/Lab";
-import Members from "./components/Members";
-import Achievements from "./components/Achievements";
-import Sponsors from "./components/Sponsors";
-import Footer from "./components/Footer";
+import { Navbar, Hero, About, Lab, Members, Achievements, Sponsors, Footer } from "./components";
 
 export default function Home() {
   return (
